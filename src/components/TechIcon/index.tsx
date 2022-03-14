27e0ab8e28467da02f69aa60b670aa222bbcb7f1
= <=>
2022-03-13 23:18:28 -0300

    tech icon component responsive

--- src/components/TechIcon/index.tsx
+++ src/components/TechIcon/index.tsx
@@ -1,4 +1,10 @@
-import { Box, Icon, Tooltip, useDisclosure } from "@chakra-ui/react";
+import {
+  Box,
+  Icon,
+  Tooltip,
+  useBreakpointValue,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { IconType } from "react-icons";
 
 interface TechIconProps {
@@ -8,6 +14,27 @@ interface TechIconProps {
 
 export function TechIcon({ icon, label }: TechIconProps) {
   const { isOpen, onClose, onOpen } = useDisclosure();
+  const isMobile = useBreakpointValue({
+    base: true,
+    md: false,
+  });
+
+  if (!isMobile) {
+    return (
+      <Tooltip label={label} fontSize="lg">
+        <Box>
+          <Icon
+            as={icon}
+            boxSize={["20", "24"]}
+            transition="0.3s"
+            _hover={{
+              transform: "scale(1.1)",
+            }}
+          />
+        </Box>
+      </Tooltip>
+    );
+  }
 
   return (
     <Tooltip label={label} fontSize="lg" isOpen={isOpen}>
@@ -15,7 +42,7 @@ export function TechIcon({ icon, label }: TechIconProps) {
         <Icon
           onClick={() => {
             onOpen();
-            setTimeout(onClose, 2500);
+            setTimeout(onClose, 2000);
           }}
           as={icon}
           boxSize={["20", "24"]}
