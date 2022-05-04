46c3ef79715a8fa8ccfe13b1f3887c5932a8b5ba
= <=>
2022-05-04 11:52:23 -0300

    apollo provider and client config

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
+import { ApolloProvider } from "@apollo/client";
 import { ColorModeScript } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { apolloClient } from "./services/apollo";
 import { theme } from "./styles/theme";
 
 ReactDOM.render(
   <React.StrictMode>
     <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-    <App />
+    <ApolloProvider client={apolloClient}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
