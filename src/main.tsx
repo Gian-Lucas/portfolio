2fb287155358d0b18d01766e3360a26489edb38b
= <=>
2022-05-04 14:05:07 -0300

    projects json

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,13 @@
-import { ApolloProvider } from "@apollo/client";
 import { ColorModeScript } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { apolloClient } from "./services/apollo";
 import { theme } from "./styles/theme";
 
 ReactDOM.render(
   <React.StrictMode>
     <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-    <ApolloProvider client={apolloClient}>
-      <App />
-    </ApolloProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
--- src/services/apollo.ts
+++
@@ -1,6 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-export const apolloClient = new ApolloClient({
-  uri: import.meta.env.VITE_GRAPHCMS_SECRET?.toString(),
-  cache: new InMemoryCache(),
-});
